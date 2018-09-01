da596de2c64285972cd7ec012449e8feb833cdc2
Kangyun Wang <kevinwang8011@hotmail.com>
2018-09-01 12:21:14 +0200

    Fix eslint errors

--- src/autoCorrelation.js
+++ src/autoCorrelation.js
@@ -1,11 +1,15 @@
 function findFundamentalFrequency(buffer, sampleRate) {
   // We use Autocorrelation to find the fundamental frequency.
 
-  // In order to correlate the signal with itself (hence the name of the algorithm), we will check two points 'k' frames away.
-  // The autocorrelation index will be the average of these products. At the same time, we normalize the values.
+  // In order to correlate the signal with itself
+  // (hence the name of the algorithm), we will check two points 'k' frames away.
+  // The autocorrelation index will be the average of these products.
+  // At the same time, we normalize the values.
   // Source: http://www.phy.mty.edu/~suits/autocorrelation.html
-  // Assuming the sample rate is 48000Hz, a 'k' equal to 1000 would correspond to a 48Hz signal (48000/1000 = 48),
-  // while a 'k' equal to 8 would correspond to a 6000Hz one, which is enough to cover most (if not all)
+  // Assuming the sample rate is 48000Hz,
+  // a 'k' equal to 1000 would correspond to a 48Hz signal (48000/1000 = 48),
+  // while a 'k' equal to 8 would correspond to a 6000Hz one,
+  // which is enough to cover most (if not all)
   // the notes we have in the notes.json file.
   const n = 1024;
   let bestK = -1;
@@ -31,7 +35,8 @@ function findFundamentalFrequency(buffer, sampleRate) {
   }
 
   if (bestR > 0.0025) {
-    // The period (in frames) of the fundamental frequency is 'bestK'. Getting the frequency from there is trivial.
+    // The period (in frames) of the fundamental frequency is 'bestK'.
+    // Getting the frequency from there is trivial.
     const fundamentalFreq = sampleRate / bestK;
     return fundamentalFreq;
   }
--- src/detector.js
+++ src/detector.js
@@ -7,18 +7,21 @@ function detectAudioContext() {
   return false;
 }
 
+function mediaPromise(constraints) {
+  return new Promise((resolve, reject) => {
+    navigator.getUserMedia(constraints, resolve, reject);
+  });
+}
+
 function detectGetUserMedia() {
   navigator.getUserMedia =		navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
   if ((navigator.mediaDevices && navigator.mediaDevices.getUserMedia) || navigator.getUserMedia) {
     return navigator.mediaDevices && navigator.mediaDevices.getUserMedia
       ? navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices)
-      : function (constraints) {
-        return new Promise((resolve, reject) => {
-          navigator.getUserMedia(constraints, resolve, reject);
-        });
-			  };
+      : mediaPromise;
   }
+
   return false;
 }
 
