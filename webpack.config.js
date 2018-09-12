19a6332a6da2487067833b8b272ce43540127625
Kangyun Wang <kevinwang8011@hotmail.com>
2018-09-12 20:59:45 +0200

    Add webpack watch script

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,34 @@ const path = require('path');
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
-  entry: './src/index.js',
-  devtool: 'source-map',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'pitch-analyser',
-    libraryTarget: 'commonjs2',
-  },
-  optimization: {
-    minimizer: [new UglifyWebpackPlugin({ sourceMap: true })],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /(node_modules|bower_components)/,
-      },
-    ],
-  },
+	entry: './src/index.js',
+	devtool: 'source-map',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		library: 'pitch-analyser',
+		libraryTarget: 'commonjs2',
+	},
+	optimization: {
+		minimizer: [],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /(node_modules|bower_components)/,
+			},
+		],
+	},
 };
 
-module.exports = config;
+module.exports = (env, argv) => {
+	if (argv.mode === 'development') {}
+
+	if (argv.mode === 'production') {
+		config.optimization.minimizer.push(new UglifyWebpackPlugin({ sourceMap: true }));
+	}
+
+	return config;
+};
