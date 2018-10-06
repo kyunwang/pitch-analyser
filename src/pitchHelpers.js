3f693b87ef6cd6a8e86cb0233b698f92589fa53f
Kangyun Wang <kevinwang8011@hotmail.com>
2018-10-06 19:50:01 +0200

    Add a checker on existing deviceid

--- src/pitchHelpers.js
+++ src/pitchHelpers.js
@@ -1,10 +1,12 @@
-const getInputDevices = () => {
-	return navigator.mediaDevices.enumerateDevices()
-		.then(devices => {
-			return devices.filter(device => device.kind === 'audioinput');
-		})
-}
+// Not used as of now
+const checkInputDeviceExists = deviceId => navigator.mediaDevices.enumerateDevices()
+	.then(devices => devices.some(device => device.deviceId === deviceId));
+
+const getInputDevices = () => navigator.mediaDevices.enumerateDevices()
+	.then(devices => devices.filter(device => device.kind === 'audioinput'));
+
 
 export {
+	checkInputDeviceExists,
 	getInputDevices,
-}
+};
