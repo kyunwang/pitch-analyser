d68105d2b16eac720ca36611aa8373e306a0e3dd
Kangyun Wang <kevinwang8011@hotmail.com>
2019-08-07 21:55:46 +0200

    Minor cleanup

--- src/analysers/MicAnalyse.js
+++ src/analysers/MicAnalyse.js
@@ -6,7 +6,6 @@ import {
 	calculateCents,
 	toDecimals,
 	throwError,
-	logError,
 } from '../helpers';
 
 let getUserMedia;
@@ -22,17 +21,17 @@ let volume;
 
 // Default of options
 let options = {
-	microphone: true,
-	audioFile: false,
+	// microphone: true,
+	// audioFile: false,
 	callback: null,
+	decimals: 2,
 	returnNote: true,
 	returnCents: false,
-	decimals: 2,
 	// afterCloseCallback() {},
 };
 
 class MicAnalyse {
-	constructor(args) {
+	constructor(args = {}) {
 		this.args = args;
 		this.lastFrequency = null;
 		this.audioContext = null;
@@ -46,9 +45,9 @@ class MicAnalyse {
 		this.initialize();
 	}
 
-	closeContext(callback) {
-		if (callback) {
-			this.audioContext.close().then(() => callback());
+	closeContext(onClose) {
+		if (onClose) {
+			this.audioContext.close().then(() => onClose());
 		}
 	}
 
@@ -57,20 +56,17 @@ class MicAnalyse {
 		this.audioContext = detectAudioContext();
 		getUserMedia = detectGetUserMedia();
 
-		if (!(this instanceof MicAnalyse)) {
-			throwError("constructor needs to be called with the 'new' keyword");
-		}
+		if (!(this instanceof MicAnalyse)) throwError("constructor needs to be called with the 'new' keyword");
+
 
 		// A callback needs to be passed during the development stage
-		if (!this.args.callback) throwError('A callback needs to be passed');
+		if (!this.args.callback) throwError('A callback needs to be passed to receive the return value');
 
 		// Does the browser support the feature? audioContext = false || window.AudioContext
-		if (!this.audioContext) {
-			logError('Your browser does not support Audio Context');
-		}
+		if (!this.audioContext) throwError('Your browser does not support Audio Context');
 
 		// getUserMedia = window.getUserMedia(went through feature detects) or false
-		if (!getUserMedia) logError('Your brower does not support getUserMedia');
+		if (!getUserMedia) throwError('Your brower does not support getUserMedia');
 
 		// Pass user given arguments to the options
 		options = { ...options, ...this.args };
